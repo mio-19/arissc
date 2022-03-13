a18fb8a6ef281d37d7fe3178803050eacb0a2b2e
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-03-13 02:56:22 +0000

    up

--- src/main/scala/TopLevelVerilog.scala
+++ src/main/scala/TopLevelVerilog.scala
@@ -1,8 +1,11 @@
-import base.C
+import base._
 import spinal.core.SpinalVerilog
 
 object TopLevelVerilog {
   def main(args: Array[String]) {
     SpinalVerilog(new C)
+    SpinalVerilog(new TH12)
+    SpinalVerilog(new TH22)
+    SpinalVerilog(new TH23)
   }
 }

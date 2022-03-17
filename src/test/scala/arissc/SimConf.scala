2469704e9019543d9bc809d6683e88af266bc695
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-03-17 06:49:10 +0000

    up

--- src/test/scala/arissc/SimConf.scala
+++ src/test/scala/arissc/SimConf.scala
@@ -4,5 +4,5 @@ import spinal.core._
 import spinal.core.sim._
 
 object SimConf {
-  val SimConf = SimConfig.withWave.withLogging.withGhdl.withConfig(SpinalConfig(mode=Verilog))
+  val SimConf = SimConfig.withWave.withLogging.withVerilator.withConfig(SpinalConfig(mode=Verilog))
 }
--- src/test/scala/arissc/base/writeChannel.scala
+++ src/test/scala/arissc/base/writeChannel.scala
@@ -7,8 +7,11 @@ import arissc.base.simutils._
 object writeChannel {
   def apply[T <: Data](ch: ChannelIn[T], data: BigInt): Unit = {
     if (!ch.isStatusWaitingOrReturningSim) throw new IllegalStateException("not empty"+simutils.getBigInt(ch.ack))
-    ch.dual ##= (data << ch.data.getBitsWidth) & (~data)
-    assert(ch.dual.isValidSim)
+    ch.dual.ones ##= data
+    ch.dual.flipped ##= ~data
+    println(ch.dual.ones.asBigInt)
+    println(ch.dual.flipped.asBigInt)
+    assert(ch.isStatusValidSim)
     waitUntil(ch.isStatusAckedSim)
     ch.dual ##= 0
     waitUntil(ch.isStatusEmptySim)

e3204bf2eb97aca5dca75d66f523a4c7894747ab
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-03-21 03:47:28 -0700

    up

--- src/main/scala/arissc/base/Regi.scala
+++ src/main/scala/arissc/base/Regi.scala
@@ -5,17 +5,11 @@ import spinal.core._
 // in1 : dual valid, ack = False; out1 : dual cleared, ack = False ;;; in1 Valid out1 Empty
 // in1 : dual valid, ack = False; out1 : ???, ack = False
 // in1 : dual valid, ack = False; out1 : dual valid, ack = False ;;; in1 Valid out1 Valid
-
-//Thread 1
-// in1 : dual valid, ack = True;
-// in1 : dual ???, ack = True;
-// in1 : dual empty, ack = True; ;;; in1 Cleared
-// in1 : dual empty, ack = False;
-
-// Thread 2
-// out1 : dual valid, ack = True; ;;; out1 Acked
-// out1 : dual ???, ack = ???
-// out1 : dual empty, ack = ???
+// in1 : dual ???, ack = True; out1 : dual valid, ack = False
+// in1 : dual ???, ack = True; out1 : dual valid, ack = True
+// in1 : dual cleared, ack = True; out1 : dual ???, ack = True
+// in1 : dual cleared, ack = True; out1 : dual cleared, ack = True
+// in1 : dual cleared, ack = False; out1 : dual cleared, ack = ???
 
 
 class Regi[T <: Data](data: T) extends Component {
@@ -24,6 +18,6 @@ class Regi[T <: Data](data: T) extends Component {
     val out1 = ChannelOut(data).noCombLoopCheck
   }
 
-  io.in1.ack := Mux(ClockDomain.current.readResetWire, False, Mux(io.in1.isStatusValid && io.out1.isStatusValid, True, Mux(io.in1.isStatusCleared, False, io.in1.ack)))
-  io.out1.dual := Mux(ClockDomain.current.readResetWire, Dual.emptyFor(io.out1.dual), Mux(io.in1.isStatusValid && io.out1.isStatusWaitingOrReturning, io.in1.dual, Mux(io.out1.isStatusAcked, Dual.emptyFor(io.out1.dual), io.out1.dual)))
+  io.in1.ack := Mux(ClockDomain.current.readResetWire, False, Mux(io.in1.isStatusValid && io.in1 === io.out1, True, Mux(io.in1.isStatusCleared && io.out1.dual.isEmpty, False, io.in1.ack)))
+  io.out1.dual := Mux(ClockDomain.current.readResetWire, Dual.emptyFor(io.out1.dual), Mux(io.out1.isStatusWaitingOrReturning, (io.in1.dual.asBits | io.out1.dual.asBits).as(Dual(data)), Mux(io.out1.isStatusAcked, Dual.emptyFor(io.out1.dual), io.out1.dual)))
 }

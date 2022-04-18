86f9262a3bac68fee8f142d61b5afea47deda434
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-04-18 06:21:36 +0000

    up

---
+++ src/test/scala/arissc/base/FIFO3BugSim.scala
@@ -0,0 +1,47 @@
+package arissc.base
+
+import arissc.SimConf.SimConf
+import arissc.base.simutils._
+import spinal.core._
+import spinal.core.sim._
+
+import scala.util.Random
+
+object FIFO3BugSim {
+  def main(args: Array[String]) {
+    val width = 64
+    SimConf.doSim(new FIFO3(UInt(width bits))) { dut =>
+      dut.io.out1.ack #= false
+      dut.io.in1.dual ##= 0
+      dut.clockDomain.assertReset()
+      sleep(1000)
+      dut.clockDomain.deassertReset()
+      sleep(1000)
+
+      {
+        val data = Random.nextInt().abs
+
+        fork {
+          writeChannel(dut.io.in1, data)
+        }
+
+        sleep(6432)
+        val got = readChannel(dut.io.out1).toInt
+        assert(got == data)
+      }
+      {
+        val data = Random.nextInt().abs
+
+        fork {
+          sleep(1234)
+          writeChannel(dut.io.in1, data)
+          print(dut.io.in1.toString)
+        }
+
+        val got = readChannel(dut.io.out1).toInt
+        assert(got == data)
+      }
+    }
+  }
+
+}
